0b92755be60dbc9802cd7b8f74cef473e16abae9
abhi16180 <4ni19cs003_a@nie.ac.in>
2022-01-20 17:32:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config(); //initialize dotenv
+
+const { default: axios } = require('axios');
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const Discord = require('discord.js'); //import discord.js
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+
+client.on('messageCreate', async message => {
+    let data = await getAPOD();
+    let content = message.content;
+    switch (content) {
+        case "/quasar_help": message.reply('Hello there,this is quasar,type apod to get image'); break;
+        case "apod": message.reply(data.url); break;
+        default: break;
+    }
+
+
+
+
+})
+
+
+async function getAPOD() {
+    let resp = await axios.get('https://api.nasa.gov/planetary/apod?api_key=tbPsHv73APCeifSzJOgY9igFFVpfUVjToit22uEV')
+    return resp.data;
+}
+//to login 
+client.login(process.env.CLIENT_TOKEN); 
